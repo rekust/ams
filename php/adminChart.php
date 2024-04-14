8472e573c628e8b32123d71f946c6585b219a4ef
“AanikethG” <aanikethghosh@gmail.com>
2024-04-14 23:07:51 +0530

    modified chart data rendering on adminChart.php file

--- php/adminChart.php
+++ php/adminChart.php
@@ -1,54 +1,49 @@
-<html>
-
 <head>
-	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-	<script type="text/javascript">
-		google.charts.load('current', {
-			'packages': ['bar']
-		});
-		google.charts.setOnLoadCallback(drawChart);
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+        google.charts.load('current', {'packages': ['bar']});
+        google.charts.setOnLoadCallback(drawChart);
 
-		function drawChart() {
-			var data = google.visualization.arrayToDataTable([
-				['Year', 'Applications', 'Rejected', 'Accepted'],
-				<?php 
-				foreach ($yearlyCounts as $year => $counts){
-				?>
-				['<?php echo $year; ?>', <?php echo $counts['total']; ?>, <?php echo $counts['status3']; ?>, <?php echo $counts['status2']; ?>],
-				<?php } ?>
-			]);
+        function drawChart() {
+            var data = new google.visualization.DataTable();
+            data.addColumn('string', 'Year');
+            data.addColumn('number', 'Applications');
+            data.addColumn('number', 'Accepted');
+            data.addColumn('number', 'Rejected');
 
-			var options = {
-				hAxis: {
-					textStyle: {
-						fontSize: 22,
-						color: '#333'
-					}
-				},
-				vAxis: {
-					textStyle: {
-						fontSize: 16,
-						color: '#333',
-						padding: 200
-					}
-				},
-				legend: {
-					textStyle: {
-						fontSize: 18,
-						color: '#333',
-					}
-				}
-			};
+            <?php foreach ($yearlyCounts as $year => $counts): ?>
+                data.addRow(['<?php echo $year; ?>', <?php echo $counts['total']; ?>, <?php echo $counts['status2']; ?>, <?php echo $counts['status3']; ?>]);
+            <?php endforeach; ?>
 
-			var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+            var options = {
+                hAxis: {
+                    textStyle: {
+                        fontSize: 22,
+                        color: '#333'
+                    }
+                },
+                vAxis: {
+                    textStyle: {
+                        fontSize: 16,
+                        color: '#333',
+                        padding: 200
+                    }
+                },
+                legend: {
+                    textStyle: {
+                        fontSize: 18,
+                        color: '#333',
+                    }
+                },
+					 colors: ['#3a86ff', '#ffbd00', '#ef233c']
+            };
 
-			chart.draw(data, google.charts.Bar.convertOptions(options));
-		}
-	</script>
+            var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+            chart.draw(data, google.charts.Bar.convertOptions(options));
+        }
+    </script>
 </head>
 
 <body>
-	<div id="columnchart_material"></div>
+    <div id="columnchart_material"></div>
 </body>
-
-</html>
